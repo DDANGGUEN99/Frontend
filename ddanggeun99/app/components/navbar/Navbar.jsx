92f4f9711d12198bf439117333295384b4bb2842
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-12 20:01:37 +0900

    Design: Design layout
    최대너비 1024px로 맞춤

--- ddanggeun99/app/components/navbar/Navbar.jsx
+++ ddanggeun99/app/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
+"use client"
+
 import React from 'react'
 
 function Navbar() {
   return (
-    <div className='fixed w-full bg-orange-500 z-10 shodow-sm text-5xl text-center text-white'>
+    <div className='fixed w-full bg-orange-500 z-10 shodow-sm text-5xl text-center text-white max-w-screen-md mx-auto'>
       DDANGGEUN99
     </div>
   )
--- ddanggeun99/app/components/tabbar/Tabbar.jsx
+++ ddanggeun99/app/components/tabbar/Tabbar.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import { BiHomeAlt } from "react-icons/bi";
 import { MdLocationCity } from "react-icons/md";
@@ -7,7 +9,7 @@ import { FiUser } from "react-icons/fi";
 
 function Tabbar() {
   return (
-    <div className="absolute h-20 bottom-0 w-full bg-white z-10 shadow-sm text-5xl text-center text-black flex justify-around">
+    <div className=" w-full absolute border-t-[3px] py-2 h-20 bottom-0 bg-white z-10 shadow-sm text-5xl text-center text-black flex justify-around  max-w-screen-md mx-auto">
       <div className="flex flex-col justify-center items-center">
         <BiHomeAlt />
         <p className="text-lg">홈</p>
