2f01f9812316e92d74102ab06c9f791b11e3eb85
KimHeeJun <khj981116@gmail.com>
2023-05-12 20:01:34 +0900

    Design: Layout width 1024px 설정

--- ddanggeun99/app/components/tabbar/Tabbar.jsx
+++ ddanggeun99/app/components/tabbar/Tabbar.jsx
@@ -7,7 +7,7 @@ import { FiUser } from "react-icons/fi";
 
 function Tabbar() {
   return (
-    <div className="absolute h-20 bottom-0 w-full bg-white z-10 shadow-sm text-5xl text-center text-black flex justify-around">
+    <div className="absolute h-20 bottom-0 w-full bg-white z-10 shadow-sm text-5xl text-center text-black flex justify-around max-w-screen-md mx-auto " >
       <div className="flex flex-col justify-center items-center">
         <BiHomeAlt />
         <p className="text-lg">홈</p>
