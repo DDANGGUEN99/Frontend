f7578a51f10e44fc17af9b861e11695845f20079
KimHeeJun <khj981116@gmail.com>
2023-05-13 00:12:30 +0900

    Fix : Navbar 스크롤 시에 고정되게 수정

--- ddanggeun99/app/components/navbar/Navbar.jsx
+++ ddanggeun99/app/components/navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
+
 function Navbar() {
   return (
-    <div className='fixed w-full bg-orange-500 z-10 shodow-sm text-5xl text-center text-white max-w-screen-md mx-auto'>
+    <div className='fixed w-full bg-orange-500 z-10 shodow-sm text-5xl text-center text-white max-w-screen-md mx-auto '>
       DDANGGEUN99
     </div>
   )
