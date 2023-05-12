9cc98ada92d021e01b948562f85f949b65cf15f5
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-12 23:25:18 +0900

    Design: Design Input component
    인풋 컴포넌트의 props에 outline속성을 추가하면 검은색 테두리가 있는 인풋창으로 변하게 조건넣음

--- ddanggeun99/app/components/inputs/Input.jsx
+++ ddanggeun99/app/components/inputs/Input.jsx
@@ -1,7 +1,16 @@
-"use client"
+"use client";
 
-
-export default function Input ({ id, disabled, type, formatPrice, errors, label }){
+export default function Input({
+  id,
+  disabled,
+  type,
+  formatPrice,
+  errors,
+  label,
+  outline,
+  register,
+  required
+}) {
   return (
     <>
       <div className="w-[400px] relative">
@@ -14,21 +23,22 @@ export default function Input ({ id, disabled, type, formatPrice, errors, label 
         <input
           id={id}
           disabled={disabled}
+          // {register(id, { required })}
           placeholder=" "
           type={type}
           className={`peer w-full p-4 pt-6 font-light bt-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed 
-        ${formatPrice ? "pl-9" : "pl-4"}
-
-
+          ${formatPrice ? "pl-9" : "pl-4"}
+          ${outline && "border-black"}
+          ${outline && "border-2"}
         `}
         />
         <label
           className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] 
-      ${formatPrice ? "left-9" : "left-4"}
-      peer-placeholder-shown:scale-100
-      peer-placeholder-shown:translate-y-0
-      peer-focus:scale-75
-      peer-focus:-translate-y-4
+          ${formatPrice ? "left-9" : "left-4"}
+          peer-placeholder-shown:scale-100
+          peer-placeholder-shown:translate-y-0
+          peer-focus:scale-75
+          peer-focus:-translate-y-4
       `}
         >
           {label}
@@ -36,4 +46,4 @@ export default function Input ({ id, disabled, type, formatPrice, errors, label 
       </div>
     </>
   );
-};
+}
