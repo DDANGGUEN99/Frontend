c8aa38ef33471cb726de461e386f54a7e8c3ba5d
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-12 18:20:23 +0900

    Add Navbar in components

---
+++ ddanggeun99/app/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='fixed w-full bg-orange-500 z-10 shodow-sm text-5xl text-center text-white'>
+      DDANGGEUN99
+    </div>
+  )
+}
+
+export default Navbar
