c7a73c27d811b225be5a5bd4141929986b9498f1
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-13 17:18:50 +0900

    Feat: Add create post function
    유저가 글쓰기 버튼을 눌렀을때 category_id, title, content, price, 유저의 location_id가 게시글에 등록됨.
    Image추가는 아직임

--- ddanggeun99/app/components/navbar/Navbar.jsx
+++ ddanggeun99/app/components/navbar/Navbar.jsx
@@ -1,26 +1,52 @@
-"use client"
+"use client";
 
-import React from 'react'
-import {ImCancelCircle} from "react-icons/im"
+import useCreatePostInput from "@/app/hooks/useCreatePostInput";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { ImCancelCircle } from "react-icons/im";
 
-function Navbar({page}) {
+function Navbar({ page }) {
+  const router = useRouter();
+
   switch (page) {
+    // createpost에 들어가는 Navbar
     case "createpost":
+      // 게시글 등록하는 함수
+      const { title, content, price, category_id } = useCreatePostInput();
+      const onSubmitPost = async () => {
+        try {
+          const newPost = {
+            title,
+            content,
+            price,
+            category_id,
+            location_id: 0,
+          };
+          await axios.post(
+            `${process.env.REACT_APP_SERVER_URL}/api/items`,
+            { newPost }, {}
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      };
       return (
-        <div className='cursor-default flex items-center justify-between px-4 fixed w-full rounded-b-lg pt-1 bg-white z-10 shodow-sm text-3xl text-center text-black max-w-screen-md mx-auto h-16 border-2'>
-          <div> <ImCancelCircle size={28}/> </div>
+        <div className="cursor-default flex items-center justify-between px-4 fixed w-full rounded-b-lg pt-1 bg-white z-10 shodow-sm text-3xl text-center text-black max-w-screen-md mx-auto h-16">
+          <div onClick={() => router.push("/main")}>
+            <ImCancelCircle size={28} />
+          </div>
           <div>내 물건 팔기</div>
-          <div className='text-orange-400 text-xl'>완료</div>
+          <div onClick={onSubmitPost} className="text-orange-400 text-xl">
+            완료
+          </div>
         </div>
-      )
-      case "mypage":
-        return (
-          <div className='cursor-default flex items-center justify-between px-4 fixed w-full rounded-b-lg pt-1 bg-orange-400 z-10 shodow-sm text-3xl text-center text-white max-w-screen-md mx-auto h-16 border-2'>
-         
-        </div>
-        )
-      break;
+      );
+    // mypage에 들어가는 Navbar
+    case "mypage":
+      return (
+        <div className="cursor-default flex items-center justify-between px-4 fixed w-full rounded-b-lg pt-1 bg-orange-400 z-10 shodow-sm text-3xl text-center text-white max-w-screen-md mx-auto h-16 border-b-2"></div>
+      );
   }
 }
 
-export default Navbar
+export default Navbar;
--- ddanggeun99/app/createpost/page.js
+++ ddanggeun99/app/createpost/page.js
@@ -1,6 +1,28 @@
+"use client";
+
+
 import Navbar from "../components/navbar/Navbar";
+import useCreatePostInput from "../hooks/useCreatePostInput";
 
 export default function CreatePost() {
+  const createPostInput = useCreatePostInput();
+
+  const handleChangeInput = (e) => {
+    switch (e.target.name) {
+      case "title":
+        createPostInput.setTitle(e.target.value);
+        break;
+      case "price":
+        createPostInput.setPrice(e.target.value);
+        break;
+      case "content":
+        createPostInput.setContent(e.target.value);
+        break;
+      case "category_id":
+        createPostInput.setCategory_Id(e.target.value);
+        break;
+    }
+  };
   return (
     <>
       <Navbar page="createpost" />
@@ -11,9 +33,38 @@ export default function CreatePost() {
           <div className="h-[100px] w-[100px] border-2">사진3</div>
           <div className="h-[100px] w-[100px] border-2">사진4</div>
         </div>
-        <div className="border-b-2 pt-6 pb-3 pl-2"><input placeholder="글 제목"/></div>
-        <div className="border-b-2 py-3 pl-2"><input placeholder="W 가격 (선택사항)"/></div>
-        <div className="border-b-2 h-40 py-3 pl-2"><input placeholder="게시글 내용을 작성해주세요."/></div>
+        <div className="border-b-2 pt-6 pb-3 pl-2">
+          <input
+            name="title"
+            onChange={handleChangeInput}
+            value={createPostInput.title}
+            placeholder="글 제목"
+          />
+        </div>
+        <div className="border-b-2 py-3 pl-2">
+          <input
+            name="price"
+            onChange={handleChangeInput}
+            value={createPostInput.price}
+            placeholder="W 가격 (선택사항)"
+          />
+        </div>
+        <div className="border-b-2 h-40 py-3 pl-2">
+          <input
+            name="content"
+            onChange={handleChangeInput}
+            value={createPostInput.content}
+            placeholder="게시글 내용을 작성해주세요."
+          />
+        </div>
+        <div className="border-b-2 h-40 py-3 pl-2">
+          <input
+            name="category_id"
+            onChange={handleChangeInput}
+            value={createPostInput.category_id}
+            placeholder="상품의 카테고리를 선택해주세요."
+          />
+        </div>
       </div>
     </>
   );
