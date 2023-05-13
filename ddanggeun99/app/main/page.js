e3f760a7161458b96d74d1586560cff788f8ce97
KimHeeJun <khj981116@gmail.com>
2023-05-14 01:06:48 +0900

    Refactor: Navbar 컴포넌트로 코드 간결화

--- ddanggeun99/app/main/page.js
+++ ddanggeun99/app/main/page.js
@@ -1,34 +1,36 @@
 import React from 'react'
 import Card from '../components/card/Card'
+import Tabbar from '../components/tabbar/Tabbar'
+import Navbar from '../components/navbar/Navbar'
+
 
 function Main() {
-  return (
-    <>
+    return (
+        <>
+            <Navbar page="main" />
 
-    {/* 상단바 */}
-    <div className="pt-[50px]">
-      <div
-        className="flex justify-between items-center p-4 border-b">
-        <div className="text-[20px]">서울</div>
-        <div className="flex items-center">
-          <button className="mr-4">검색</button>
-          <button className="mr-4">카테고리</button>
-          <button>알림</button>
-        </div>
-      </div>
-    </div>
+            {/* 카드 반복 부분 */}
+            <div className='mt-16'>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+
+            {/* 글쓰기 버튼 */}
+            <div className='sticky bottom-24 self-end mr-6'>
+                <div className="cursor-pointer text-center w-32 bg-orange-400 text-white text-xl rounded-2xl p-4 ">+ 글쓰기</div>
+            </div>
 
 
-    {/* 글쓰기 버튼 */}
-    <button className="fixed right-[360px] top-[650px] m-4 bg-orange-500 text-white h-[50px] w-[150px] rounded-3xl" >+ 글쓰기 </button>
-
-    {/* 카드 반복 부분 */}
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-  </>
-  )
+            <Tabbar page="main" />
+        </>
+    )
 }
 
 export default Main
