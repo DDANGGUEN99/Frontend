2209b9ec90d4fe112aa095fdb678b9e29ad77300
KimHeeJun <khj981116@gmail.com>
2023-05-13 14:09:58 +0900

    Add: 프로필 수정 페이지 추가

--- ddanggeun99/app/components/tabbar/Tabbar.jsx
+++ ddanggeun99/app/components/tabbar/Tabbar.jsx
@@ -5,7 +5,10 @@ import { TiLocationOutline } from "react-icons/ti";
 import { BsChat } from "react-icons/bs";
 import { FiUser } from "react-icons/fi";
 
+
+
 function Tabbar() {
+
   return (
     <div className="fixed h-20 bottom-0 w-full bg-white z-10 shadow-sm text-5xl text-center text-black flex justify-around max-w-screen-md mx-auto border-t-[2px] pt-2" >
       <div className="flex flex-col justify-center items-center">
@@ -25,8 +28,8 @@ function Tabbar() {
         <p className="text-lg"> 채팅</p>
       </div>
       <div className="flex flex-col justify-center items-center">
-        <FiUser />
-        <p className="text-lg">나의 당근</p>
+          <FiUser />
+          <p className="text-lg">나의 당근</p>
       </div>
     </div>
   );
