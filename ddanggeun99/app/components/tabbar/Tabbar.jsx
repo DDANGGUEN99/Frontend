ec8288fa3e9a589830ac555589635d83cae51b35
KimHeeJun <khj981116@gmail.com>
2023-05-14 01:05:47 +0900

    Refactor: 컴포넌트로 코드 간결화

--- ddanggeun99/app/components/tabbar/Tabbar.jsx
+++ ddanggeun99/app/components/tabbar/Tabbar.jsx
@@ -6,35 +6,38 @@ import { MdLocationCity } from "react-icons/md";
 import { TiLocationOutline } from "react-icons/ti";
 import { BsChat } from "react-icons/bs";
 import { FiUser } from "react-icons/fi";
+import MypageLink from "./MypageLink";
 
 
 
-function Tabbar() {
-
-  return (
-    <div className=" w-full fixed border-t-[2px] py-2 h-20 bottom-0 bg-white z-10 shadow-sm text-5xl text-center text-black flex justify-around  max-w-screen-md mx-auto">
-      <div className="flex flex-col justify-center items-center">
-        <BiHomeAlt />
-        <p className="text-lg">홈</p>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <MdLocationCity />
-        <p className="text-lg">동네생활</p>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <TiLocationOutline />
-        <p className="text-lg">내 근처</p>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <BsChat />
-        <p className="text-lg"> 채팅</p>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-          <FiUser />
-          <p className="text-lg">나의 당근</p>
-      </div>
-    </div>
-  );
+function Tabbar({page}) {
+  switch(page){
+    case "main":
+      return(
+          <div className=" w-full fixed border-t-[2px] py-2 h-20 bottom-0 bg-white z-10 shadow-sm text-5xl text-center text-black flex justify-around  max-w-screen-md mx-auto self-center border-x">
+            <div className="flex flex-col justify-center items-center">
+              <BiHomeAlt />
+              <p className="text-lg">홈</p>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <MdLocationCity />
+              <p className="text-lg">동네생활</p>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <TiLocationOutline />
+              <p className="text-lg">내 근처</p>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <BsChat />
+              <p className="text-lg"> 채팅</p>
+            </div>
+            <MypageLink/>
+          </div>
+      )
+      break;
+  }
+ 
+  ;
 }
 
 export default Tabbar;
