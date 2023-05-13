63e238a8ed9866b8f5912efe01dfed5f2ebebafb
KimHeeJun <khj981116@gmail.com>
2023-05-14 01:04:24 +0900

    Refactor: Navbar case 추가

--- ddanggeun99/app/components/navbar/Navbar.jsx
+++ ddanggeun99/app/components/navbar/Navbar.jsx
@@ -2,24 +2,33 @@
 
 
 import React from 'react'
-import {ImCancelCircle} from "react-icons/im"
+import { ImCancelCircle } from "react-icons/im"
+import { AiOutlineSearch } from "react-icons/ai"
+import { BsList, BsBell } from "react-icons/bs"
 
-function Navbar({page}) {
+
+
+function Navbar({ page }) {
   switch (page) {
     case "createpost":
       return (
-        <div className='cursor-default flex items-center justify-between px-4 fixed w-full rounded-b-lg pt-1 bg-white z-10 shodow-sm text-3xl text-center text-black max-w-screen-md mx-auto h-16 border-2'>
-          <div> <ImCancelCircle size={28}/> </div>
+        <div className='cursor-default flex items-center justify-between px-4 fixed w-full bg-white z-10 shodow-sm text-3xl text-center text-black max-w-screen-md mx-auto h-16 border-2 self-center'>
+          <div> <ImCancelCircle size={28} /> </div>
           <div>내 물건 팔기</div>
           <div className='text-orange-400 text-xl'>완료</div>
         </div>
       )
-      case "mypage":
-        return (
-          <div className='cursor-default flex items-center justify-between px-4 fixed w-full rounded-b-lg pt-1 bg-orange-400 z-10 shodow-sm text-3xl text-center text-white max-w-screen-md mx-auto h-16 border-2'>
-         
+    case "main":
+      return (
+        <div className='cursor-default flex items-center justify-between px-4 fixed w-full bg-white z-10 shodow-sm text-xl text-center text-black max-w-screen-md mx-auto h-16 border-2 self-center'>
+          <div className="text-[20px]">서울</div>
+          <div className="flex items-center">
+            <button className="mr-4"><AiOutlineSearch size={"25px"}/></button>
+            <button className="mr-4"><BsList size={"25px"}/></button>
+            <button><BsBell size={"22px"}/></button>
+          </div>
         </div>
-        )
+      )
       break;
   }
 }
