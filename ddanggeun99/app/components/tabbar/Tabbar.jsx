15a98ba3f74a1afe28837183c71e2fc91a8f68bf
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-13 16:35:46 +0900

    Feat: Feat when user press tabbar
    유저가 탭바를 눌렀을때 라우팅이 되도록 했음

--- ddanggeun99/app/components/tabbar/Tabbar.jsx
+++ ddanggeun99/app/components/tabbar/Tabbar.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import { BiHomeAlt } from "react-icons/bi";
@@ -6,32 +6,41 @@ import { MdLocationCity } from "react-icons/md";
 import { TiLocationOutline } from "react-icons/ti";
 import { BsChat } from "react-icons/bs";
 import { FiUser } from "react-icons/fi";
-
-
+import { useRouter } from "next/navigation";
 
 function Tabbar() {
-
+  const router = useRouter();
   return (
     <div className=" w-full fixed border-t-[2px] py-2 h-20 bottom-0 bg-white z-10 shadow-sm text-5xl text-center text-black flex justify-around  max-w-screen-md mx-auto">
-      <div className="flex flex-col justify-center items-center">
+      <div
+        onClick={() => router.push("/main")}
+        className="flex flex-col justify-center items-center cursor-pointer"
+      >
         <BiHomeAlt />
         <p className="text-lg">홈</p>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      
+      <div className="flex flex-col justify-center items-center cursor-pointer">
         <MdLocationCity />
         <p className="text-lg">동네생활</p>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      
+      <div className="flex flex-col justify-center items-center cursor-pointer">
         <TiLocationOutline />
         <p className="text-lg">내 근처</p>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      
+      <div className="flex flex-col justify-center items-center cursor-pointer">
         <BsChat />
         <p className="text-lg"> 채팅</p>
       </div>
-      <div className="flex flex-col justify-center items-center">
-          <FiUser />
-          <p className="text-lg">나의 당근</p>
+      
+      <div
+        onClick={() => router.push("/mypage")}
+        className="flex flex-col justify-center items-center cursor-pointer"
+      >
+        <FiUser />
+        <p className="text-lg">나의 당근</p>
       </div>
     </div>
   );
