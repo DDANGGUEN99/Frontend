0fd7ff88d50102f2fd3d64dc98e7ec22a2f1341c
KimHeeJun <khj981116@gmail.com>
2023-05-14 00:54:26 +0900

    Design: Card height remove

--- ddanggeun99/app/components/card/Card.jsx
+++ ddanggeun99/app/components/card/Card.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 function Card() {
     return (
-        <div className="h-[150px] flex items-center border-b p-[10px]" >
-            <div className="w-[130px] ml-[10px]" >
-                <img src="https://mcafethumb-phinf.pstatic.net/MjAyMTA0MThfMjM2/MDAxNjE4NzA1MDk0MTI4.wcKUvg5WFihAqaFQ2VJdcZwZTpv44Az3fuESE_aWCoQg.RWb4KGUHvbXQwCmMIvR1ZXQ285maRoDpPKc0683xOZsg.JPEG/1C206A79-4EE4-40E0-B6C4-5EB34731E84E.jpeg?type=w800"
-                    style={{ borderRadius: "10px" }} />
+            <div className="flex items-center border-b p-[10px] border-x" >
+                <div className="w-[130px] ml-[10px]" >
+                    <img src="https://mcafethumb-phinf.pstatic.net/MjAyMTA0MThfMjM2/MDAxNjE4NzA1MDk0MTI4.wcKUvg5WFihAqaFQ2VJdcZwZTpv44Az3fuESE_aWCoQg.RWb4KGUHvbXQwCmMIvR1ZXQ285maRoDpPKc0683xOZsg.JPEG/1C206A79-4EE4-40E0-B6C4-5EB34731E84E.jpeg?type=w800"
+                        style={{ borderRadius: "10px" }} />
+                </div>
+                <div className="ml-[10px] mb-[10px]">
+                    <div className="mb-2">제목 : 닌텐도 스위치 팝니다</div>
+                    <div className="text-[12px] mb-2">서울 ・ 24분 전</div>
+                    <div>300,000원</div>
+                </div>
             </div>
-            <div className="ml-[10px] mb-[10px]">
-                <div className="mb-2">제목 : 닌텐도 스위치 팝니다</div>
-                <div className="text-[12px] mb-2">서울 ・ 24분 전</div>
-                <div>300,000원</div>
-            </div>
-        </div>
     )
 }
 
