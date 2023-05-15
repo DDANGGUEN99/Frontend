2a57900020f788fa28c0e0b2094a413323f06084
KimHeeJun <khj981116@gmail.com>
2023-05-15 11:04:46 +0900

    Feat: 글쓰기 버튼 클릭 시 글쓰기 페이지로 이동

--- ddanggeun99/app/main/page.js
+++ ddanggeun99/app/main/page.js
@@ -1,10 +1,15 @@
+"use client";
+
 import React from 'react'
 import Card from '../components/card/Card'
 import Tabbar from '../components/tabbar/Tabbar'
 import Navbar from '../components/navbar/Navbar'
+import { useRouter } from 'next/navigation';
 
 
 function Main() {
+    const router = useRouter()
+
     return (
         <>
             <Navbar page="main" />
@@ -24,7 +29,7 @@ function Main() {
 
             {/* 글쓰기 버튼 */}
             <div className='sticky bottom-24 self-end mr-6'>
-                <div className="cursor-pointer text-center w-32 bg-orange-400 text-white text-xl rounded-full p-4 ">+ 글쓰기</div>
+                <div onClick={()=>{router.push('/createpost')}} className="cursor-pointer text-center w-32 bg-orange-400 text-white text-xl rounded-full p-4">+ 글쓰기</div>
             </div>
 
 
