4a633304c5ecbaf92fe403d1de66c949f75249fb
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-15 11:50:48 +0900

    Feat: Add function add post
    글쓰기 추가 기능 api작성 완료.
    
    명세서대로 했지만 404오류가 뜨는 현상 확인

--- ddanggeun99/app/main/page.js
+++ ddanggeun99/app/main/page.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import Card from '../components/card/Card'
-import Tabbar from '../components/tabbar/Tabbar'
-import Navbar from '../components/navbar/Navbar'
+"use client"
 
+import React from "react";
+import Card from "../components/card/Card";
+import Tabbar from "../components/tabbar/Tabbar";
+import Navbar from "../components/navbar/Navbar";
+import { useRouter } from "next/navigation";
 
 function Main() {
-    return (
-        <>
-            <Navbar page="main" />
+  const router = useRouter();
+  return (
+    <>
+      <Navbar page="main" />
 
-            {/* 카드 반복 부분 */}
-            <div className='mt-16'>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-            </div>
+      {/* 카드 반복 부분 */}
+      <div className="mt-16">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
 
-            {/* 글쓰기 버튼 */}
-            <div className='sticky bottom-24 self-end mr-6'>
-                <div className="cursor-pointer text-center w-32 bg-orange-400 text-white text-xl rounded-full p-4 ">+ 글쓰기</div>
-            </div>
+      {/* 글쓰기 버튼 */}
+      <div className="sticky bottom-24 self-end mr-6">
+        <div
+          onClick={() => router.push("/createpost")}
+          className="cursor-pointer text-center w-32 bg-orange-400 text-white text-xl rounded-full p-4 "
+        >
+          + 글쓰기
+        </div>
+      </div>
 
-
-            <Tabbar page="main" />
-        </>
-    )
+      <Tabbar page="main" />
+    </>
+  );
 }
 
-export default Main
+export default Main;
