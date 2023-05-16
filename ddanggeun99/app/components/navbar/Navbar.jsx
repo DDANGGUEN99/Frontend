3b27db66b7d526aa9ae6cb83d3bc8d5190a15280
KimHeeJun <khj981116@gmail.com>
2023-05-16 14:28:33 +0900

    Design: Navbar 디자인 변경

--- ddanggeun99/app/components/navbar/Navbar.jsx
+++ ddanggeun99/app/components/navbar/Navbar.jsx
@@ -6,13 +6,12 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { BsList, BsBell } from "react-icons/bs";
 import { RxCaretDown } from "react-icons/rx";
 import Cookies from "js-cookie";
-import { MdOutlineNavigateBefore } from "react-icons/md"
-import { useRouter } from 'next/navigation'
 
-
+import axios from "axios";
+import useCloudinaryUrl from "../../hooks/useCloudinaryUrl";
+import useCreatePostInput from "../../hooks/useCreatePostInput";
 
 function Navbar({ page }) {
-  const router = useRouter()
   switch (page) {
     case "createpost":
       const cloudinaryUrl = useCloudinaryUrl
@@ -54,44 +53,8 @@ function Navbar({ page }) {
             <ImCancelCircle size={28} />{" "}
           </div>
           <div>내 물건 팔기</div>
-          <div className='text-orange-400 text-xl'>완료</div>
-        </div>
-      )
-    case "main":
-      return (
-        <div className='cursor-default flex items-center justify-between px-4 fixed w-full bg-white z-10 shodow-sm text-xl text-center text-black max-w-screen-md mx-auto h-16 border-2 self-center'>
-          <div className="text-[20px] flex items-center ml-2">서울<RxCaretDown size={"30px"} /></div>
-          <div className="flex items-center">
-            <button className="mr-4"><AiOutlineSearch size={"25px"} /></button>
-            <button className="mr-4"><BsList size={"25px"} /></button>
-            <button><BsBell size={"22px"} /></button>
-          </div>
-        </div>
-      )
-    case "interest":
-      return (
-        <div className="cursor-default flex items-center px-4 fixed w-full bg-white z-10 shadow-sm text-xl text-center text-black max-w-screen-md mx-auto h-16 border-2 self-center">
-          <button onClick={() => { router.push('/mypage') }}><MdOutlineNavigateBefore size={"35px"} /></button>
-          <div className="flex items-center justify-center w-full">
-            <div className="text-2xl ml-2">관심목록</div>
-          </div>
-        </div>
-      )
-    case "orderHistory":
-      return (
-        <div className="cursor-default flex items-center px-4 fixed w-full bg-white z-10 shadow-sm text-xl text-center text-black max-w-screen-md mx-auto h-16 border-2 self-center">
-          <button onClick={() => { router.push('/mypage') }}><MdOutlineNavigateBefore size={"35px"} /></button>
-          <div className="flex items-center justify-center w-full">
-            <div className="text-2xl ml-2">구매내역</div>
-          </div>
-        </div>
-      )
-      case "salesHistory":
-      return (
-        <div className="cursor-default flex items-center px-4 fixed w-full bg-white z-10 shadow-sm text-xl text-center text-black max-w-screen-md mx-auto h-16 border-2 self-center">
-          <button onClick={() => { router.push('/mypage') }}><MdOutlineNavigateBefore size={"35px"} /></button>
-          <div className="flex items-center justify-center w-full">
-            <div className="text-2xl ml-2">판매내역</div>
+          <div onClick={submitCreatePost} className="text-orange-400 text-xl">
+            완료
           </div>
         </div>
       );
